d1ed1c1c15e02755e77d0f2f995a9b490ff66105
Nelson Nguyen <nelsonnguyen@Mac.lan>
2025-09-23 23:52:39 -0700

    Added weather update

--- nelson_disc_bot.js
+++ nelson_disc_bot.js
@@ -20,19 +20,35 @@ const client = new Client({
     intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages]
 });
 
+//VARIABLES
 const CHANNEL_ID = "1419385686037106789" // will fill with channel code
 // go to developer mode in Discord, right-clic channel, copy channelID
+const CITY = "Portland";
+const apiKey = process.env.API_KEY;
 
 client.once("ready", () => {
     console.log('Logged in as ${client.user.tag}');
-
-    // Senda  message every 60 seconds
-    setInterval(() => {
+    setInterval(async () => {
+        
         const channel = client.channels.cache.get(CHANNEL_ID);
-        if (channel) {
-            channel.send("You are gay !");
+        if(!channel) return; // returns if no channel is found
+
+        try {
+            const response = await fetch(
+                `https://api.openweathermap.org/data/2.5/weather?q=${CITY}&appid=${apiKey}&units=imperial`
+            );
+            const data = await response.json();
+
+            if(data.cod === 200) {
+                const weatherMessage = `🌤️ Weather in ${data.name}: ${data.main.temp}°F, ${data.weather[0].description}`;
+                channel.send(weatherMessage);
+            } else {
+                channel.send("❌ Could not fetch weather data.");
+            }
+        } catch (error) {
+            console.error("Weather API error:", error);
         }
-    }, 0.1 * 1000); // 60 seconds
+    }, 60 * 60 * 1000); // 60 seconds
 });
 
 // bot logs in using the DISCORD TOKEN in the .env file

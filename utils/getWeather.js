1f760f86e8f957853a32bd2314fc9d4924facecd
Nelson Nguyen <nelsonnguyen9878@gmail.com>
2025-09-26 00:17:09 -0700

    fixed API error issue with parseInt(data.cod, 10)

--- utils/getWeather.js
+++ utils/getWeather.js
@@ -1,4 +1,4 @@
-
+const fetch = require("node-fetch"); // if Node < 18
 
 async function getWeather(city = process.env.CITY) {
     const apiKey = process.env.API_KEY;
@@ -8,10 +8,16 @@ async function getWeather(city = process.env.CITY) {
     const response = await fetch(url);
     const data = await response.json();
 
-    if (data != 200) {
-        throw new Error("Weather API Error");
+    // debugging: API response
+    //console.log("API response:", data);
+
+    //!data.cod ensures the property exists
+    //using parseInt(data.cod,10) handles both string and number form
+    if (!data.cod || parseInt(data.cod, 10) !== 200) {
+        throw new Error(data.message || "Weather API Error");
     }
 
+    // weather message displayed in discord when using slash command
     return `🌤️ Weather in ${data.name}:
         Temp: ${data.main.temp}°F (Feels like ${data.main.feels_like}°F)
         Humidity: ${data.main.humidity}%
